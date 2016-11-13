43984295d35ec210df99ec94c611583c3bc99c0d
Sridhar Ratnakumar <happyandharmless@icloud.com>
2016-11-13 13:18:22 +0530

    View for hand

--- src/Pallanguzhi/Game/View.elm
+++ src/Pallanguzhi/Game/View.elm
@@ -1,17 +1,35 @@
 module Pallanguzhi.Game.View exposing (view)
 
+import Maybe
 import Html exposing (..)
 
 import Pallanguzhi.Game.Model as Model
 import Pallanguzhi.Board.View as BoardView
 
+mapMaybe : (a -> Html b) -> Maybe a -> Html b
+mapMaybe f =
+  Maybe.map f >> Maybe.withDefault (text "Nothing")
+
 view : Model.Model -> Maybe String -> Html Model.Msg
 view model errorMaybe =
   let
-    boardHtml = BoardView.viewC Model.Play model.board 
-    errorHtml = viewError errorMaybe
+    boardHtml = 
+      BoardView.viewC Model.Play model.board 
+    handHtml = 
+      mapMaybe viewHand model.hand
+    errorHtml = 
+      viewError errorMaybe
   in
-  div [] [boardHtml, errorHtml]
+  div [] [boardHtml, handHtml, errorHtml]
+
+viewHand : Model.Hand -> Html Model.Msg
+viewHand hand =
+  div [] 
+    [ hr [] [] 
+    , b [] [ text <| "Player: " ++ toString hand.player ]
+    , span [] [ text <| ", Seeds: " ++ toString hand.seeds]
+    , span [] [ text <| ", Pit: " ++ toString hand.loc]
+    ]
 
 viewError : Maybe String -> Html a
 viewError errorMaybe =
