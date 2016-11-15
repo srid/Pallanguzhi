9c3c5e406f3999af9f4c2bcff3bcbc07b9d2ec87
Sridhar Ratnakumar <happyandharmless@icloud.com>
2016-11-15 12:23:27 +0530

    Working SvgView!

--- src/Pallanguzhi/Board/View.elm
+++ src/Pallanguzhi/Board/View.elm
@@ -1,14 +1,17 @@
 module Pallanguzhi.Board.View exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
-import Css
-import Css exposing (hex)
+import Html
+import Html exposing (Html)
+import Svg exposing (..)
+import Svg.Attributes as S
+import Svg.Events exposing (..)
+import List
+import Debug
+
+import Util.Diagram as D
 
 import Pallanguzhi.Board.Model exposing (Model)
 import Pallanguzhi.Board.Model as Model
-import Pallanguzhi.Board.SvgView as SvgView
 
 type alias PitClickF a
   =  Model.Player 
@@ -16,46 +19,71 @@ type alias PitClickF a
   -> a 
 
 viewC : PitClickF a -> Model -> Html a
-viewC f board =
-  let
-    (pitsA, pitsB) = Model.rows board
+viewC f board = 
+  let 
+    svgMeta = 
+      [ S.version "1.1", S.x "0", S.y "0", S.viewBox "0 0 300 100"]
+    drawing = 
+      viewBoard f board
   in 
-    div []
-      [ viewStore board.storeA
-      , hr [] []
-      , viewPits f Model.A pitsA
-      , hr [] []
-      , viewPits f Model.B (List.reverse pitsA)
-      , hr [] []
-      , viewStore board.storeB
-      , hr [] []
-      , SvgView.view (f Model.A)
-      ]
+    svg svgMeta (drawing |> D.draw 2 2)
 
-viewPits : PitClickF a -> Model.Player -> List Model.Pit -> Html a
+viewBoard : PitClickF a -> Model ->  D.Diagram a
+viewBoard f board =
+  let 
+    (rowA, rowB) =
+      let 
+        (a, b) = Model.rows board
+      in
+        (viewPits f Model.A a, viewPits f Model.B b)
+    makeStore =
+      viewStore (D.width rowA) 
+  in 
+    D.vfold 5 [makeStore board.storeB, rowB, rowA, makeStore board.storeA]
+
+viewPits : PitClickF a -> Model.Player -> List Model.Pit -> D.Diagram a
 viewPits f player =
-  div [] << List.indexedMap (viewPit f player)
-
-viewPit : PitClickF a -> Model.Player -> Int -> Model.Pit -> Html a
-viewPit f player pitLoc pit =
-  let
-    s = styles
-          [ Css.backgroundColor <| hex "11ff00"
-          , Css.padding <| Css.em 1
-          , Css.margin <| Css.px 1 ]
+  List.indexedMap (viewPit f player) 
+  >> (case player of
+      Model.A -> 
+        identity
+      Model.B ->  -- Reverse second player row (top) due to going counter clockwise
+        List.reverse)
+  >> D.hfold 5
+        
+    
+viewPit : PitClickF a -> Model.Player -> Model.PitLocation -> Model.Pit -> D.Diagram a
+viewPit f player loc pit =
+  let 
+    radius = 
+      12
+    color = 
+      "#60B5CC"
+    seed dx dy = 
+      D.circle [] 2 |> D.move (5 + dx) (7 + dy)
+    seeds =
+      -- XXX: one pit can have *more than* 6 seeds
+      -- Perhaps make this generic for N number of seeds, with N displayed in a corner.
+      let 
+        row = (D.hfold 1 <| List.repeat 3 <| seed 0 0)
+      in
+        D.vfold 2 [row, row]
+    circle = 
+      -- FIXME: onClick should be set on text as well.
+      D.circle [S.fill color, onClick (f player loc)] radius
+    text =
+      (D.text [S.fontSize "12"] (toString pit.seeds) |> D.move 5 16)  -- FIXME: make fixed size
   in
-    span 
-      [s, onClick <| f player pitLoc] 
-      [text <| toString pit.seeds]
+    if pit.player == player then
+      D.stack circle text
+    else
+      Debug.crash "incorrect player"
 
-viewStore : Int -> Html a
-viewStore seeds = 
-  let s = styles 
-            [ Css.backgroundColor <| hex "aa3300"
-            , Css.padding <| Css.em 1 ]
+viewStore : Int -> Int -> D.Diagram a
+viewStore w seeds =
+  let 
+    h = 12
+    g = D.rect [S.fill "#44ee55"] w h 
+    t = D.text [S.fontSize "12"] (toString seeds) |> D.move 90 10
   in
-    div [s] [text <| toString seeds]
-
-styles : List Css.Mixin -> Attribute a
-styles =
-    Css.asPairs >> Html.Attributes.style
+    D.stack g t 
