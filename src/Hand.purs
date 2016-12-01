679ffd56bc17a3ba743159c836996273fd08d3d9
Sridhar Ratnakumar <srid@srid.ca>
2016-12-01 18:27:23 +0530

    Get rid of Turn terminology confusion
    Board is also part if Hand now. Everything is abstracted away in Hand.
    I’m not sure if this is the best.

--- src/Hand.purs
+++ src/Hand.purs
@@ -1,17 +1,61 @@
 -- / What is in the hand during a round
-module App.Hand where 
+module App.Hand where
 
-import App.Board as Board 
+import Data.Maybe
+import App.Board as Board
+import Data.Function (apply)
+import Data.List (List(..), (:), concat, foldr)
+import Data.Tuple (Tuple(..))
+import Data.Unfoldable (class Unfoldable, unfoldr)
+import Prelude ((>>>), (<<<), ($), map)
 
-type State = 
-  { player :: Board.Player 
+type State =
+  { player :: Board.Player
   , seeds :: Board.Cell
   , pitRef :: Board.PitRef
+  , board :: Board.State
   }
 
-init :: Board.Player -> Board.Cell -> State 
-init player seeds = 
+init :: Board.Player -> Board.PitRef -> Board.State -> State
+init player pitRef board =
   { player: player
-  , seeds: seeds 
-  , pitRef: Board.makeRef player 0 
+  , seeds: 0
+  , pitRef: pitRef
+  , board: board
   }
+
+unfoldTurns :: State -> List (State -> State)
+unfoldTurns = concat <<< unfoldr' nextTurns
+
+nextTurns :: State -> Tuple (List (State -> State)) (Maybe State)
+nextTurns state@{ player, seeds, pitRef, board } =
+  go seeds
+     (Board.lookup pitRef board)
+     (Board.lookup (Board.nextRef pitRef) board)
+     (Board.lookup ((Board.nextRef <<< Board.nextRef) pitRef) board)
+     state
+  where continue xs = Tuple xs (Just $ applyTurns xs state)
+        end xs = Tuple xs Nothing
+        go 0 0 0 _ state' =
+          end $ Nil
+        go 0 0 s _ state' =
+          end $ advance : capture : Nil
+        go _ _ _ _ state' =
+          end $ Nil -- TODO: signal not reachable state
+
+applyTurns :: List (State -> State) -> State -> State
+applyTurns turns s = foldr apply s turns
+
+advance :: State -> State
+advance state =
+  state -- TODO
+
+capture :: State -> State
+capture state@{ player, seeds, pitRef, board } =
+  state { board = board' }
+  where board' = ( Board.clear pitRef >>> Board.store player seeds' ) board
+        seeds'  = Board.lookup pitRef board
+-- | A version of unfoldr that allows a elements in end cause.
+unfoldr' :: forall a b t. Unfoldable t
+        => (b -> Tuple a (Maybe b)) -> b -> t a
+unfoldr' f = unfoldr (map f) <<< Just
--- src/Round.purs
+++ src/Round.purs
@@ -3,37 +3,44 @@ module App.Round where
 
 import Data.Maybe (Maybe(..))
 import App.Board as Board
-import App.Turn as Turn
+import App.Hand as Hand
+import App.Animation as Animation
+import Prelude (($))
 
 data State
-  = Sowing Turn.State
+  = Sowing (Animation.State Hand.State)
   | Awaiting Board.Player Board.State
 
 data Action
-  = TurnAction Turn.Action
+  = AnimateTurn
   | PlayerSelect Board.PitRef
 
 init :: Board.Player -> Board.State -> State
 init player = Awaiting player
 
+sow :: Board.Player -> Board.PitRef -> Board.State -> State
+sow player pitRef board = Sowing $ Animation.init hand rest
+  where hand = Hand.init player pitRef board
+        rest = Hand.unfoldTurns hand
+
 -- XXX: Should this be made a type class?
 getBoard :: State -> Board.State
-getBoard (Sowing turnA) = turnA.current.board
+getBoard (Sowing handA) = handA.current.board
 getBoard (Awaiting _ board) = board
 
 update :: Action -> State -> State
-update (TurnAction action) (Sowing turnA) =
+update AnimateTurn (Sowing handA) =
   -- XXX: can this pattern be abstracted out?
   -- "update inner state, but if it is Nothing do this"
-  case Turn.update action turnA of
+  case Animation.step handA of
     Nothing -> -- End of turn.
       -- TODO: sould we end the round itself?
-      let opponent = Board.opponentOf turnA.current.hand.player
-      in Awaiting opponent turnA.current.board
-    Just turnA' ->
-      Sowing turnA'
+      let opponent = Board.opponentOf handA.current.player
+      in Awaiting opponent handA.current.board
+    Just handA' ->
+      Sowing handA'
 update (PlayerSelect pitRef) (Awaiting player board) =
-  Sowing (Turn.init player board)
+  sow player pitRef board
 update _ state =
   -- TODO: make this state transition impossible.
   state
