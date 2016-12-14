9d6c92c9cfe8c47d7fe3c98b6dc60d6213d80ce8
Sridhar Ratnakumar <srid@srid.ca>
2016-12-14 16:46:24 +0530

    Resolve display hacks

--- src/BoardView.purs
+++ src/BoardView.purs
@@ -1,10 +1,12 @@
 module App.BoardView where 
 
 import App.FixedMatrix72 as FM
+import Pux.CSS as C
 import App.Board (Pit, PitRef, Player, Board, getStore)
 import App.FixedMatrix72 (Row(B, A))
-import Prelude (bind, const, show, ($), (<>))
-import Pux.CSS (Color, backgroundColor, em, hsl, lighten, padding, rotateHue, style)
+import Prelude (bind, const, show, ($), (<>), (<))
+import Pux.CSS (Color, backgroundColor, border, display, em, px, flexFlow, fontSize, hsl, inline, left, lighten, padding, pct, rotateHue, style, width)
+import Pux.Html as H
 import Pux.Html (Html, div, hr, span, text)
 import Pux.Html.Events (onClick)
 
@@ -27,22 +29,21 @@ view :: forall action state. BoardView state
 view f state =
   div []
   [ viewStore state A
-  , hr [] [] -- XXX: remove this ugly display hack
-  , div [] $ viewRow A
-  , hr [] [] -- XXX: remove this ugly display hack
-  , div [] $ viewRow B
-  , hr [] [] -- XXX: remove this ugly display hack
+  , viewRow A
+  , viewRow B
   , viewStore state B
   ]
   where
     board = getBoard state
-    viewRow player = FM.mapRowWithIndex player viewCell' board.cells
-      where viewCell' ref = viewCell state (f ref) ref 
+    viewRow player = div [css] $ rows 
+      where rows = FM.mapRowWithIndex player viewCell' board.cells
+            viewCell' ref = viewCell state (f ref) ref 
+            css = style $ do 
+              apply4 C.padding (em 0.5)
 
 viewStore :: forall a state. BoardView state 
           => state -> Row -> Html a
-viewStore state player = 
-  div [css] [text s] 
+viewStore state player = div [css] [text s] 
     where s = "Player " 
                 <> show player
                 <> " with "
@@ -60,15 +61,24 @@ viewStore state player =
 viewCell :: forall a state. BoardView state 
           => state -> a -> PitRef -> Pit -> Html a
 viewCell state action ref count =
-  span [css, event] [body]
+  H.pre [css, event] [body]
   where
-    body = text $ show count
+    body = text $ numberPadded count
     event = onClick $ const action
+    numberPadded num = 
+      if num < 10 
+        then " " <> show num 
+        else show num
     css = style $ do
-      backgroundColor color
-      apply4 padding (em 0.9)
+      C.display C.inline
+      C.fontSize (em 2.0)
+      C.backgroundColor color
+      C.padding (em 0.0) (em padding) (em 0.0) (em padding)
+      C.border C.solid (px 1.0) C.black
       where
         color = pitStateColor $ pitState state ref
+        padding = 0.5
 
 apply4 :: forall a b. (a -> a -> a -> a -> b) -> a -> b
 apply4 f a = f a a a a
+
